4ab8718a7ae6f8c8845623e5fe3f5e7e6d8061e0
VadymDenysiuk <80970410371q@gmail.com>
2024-08-10 14:55:23 +0300

    add guard strategy

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,10 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { CreateUserDTO } from '../users/dto';
 import { UserLoginDTO } from './dto';
 import { AuthUserResponse } from './response';
 import { ApiResponse, ApiTags } from '@nestjs/swagger';
+import { jwtAuthGuard } from 'src/guards/jwt-guard';
 
 @Controller('auth')
 export class AuthController {
@@ -21,5 +22,11 @@ export class AuthController {
   @Post('login')
   login(@Body() dto: UserLoginDTO): Promise<UserLoginDTO> {
     return this.authService.loginUser(dto);
+  }
+
+  @UseGuards(jwtAuthGuard)
+  @Post('test')
+  test(): boolean {
+    return true;
   }
 }
