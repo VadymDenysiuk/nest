2dff08313f86e9d03e98967164a40f0a67c01dbe
VadymDenysiuk <80970410371q@gmail.com>
2024-08-10 13:34:27 +0300

    add login

---
+++ src/modules/users/users.controller.ts
@@ -0,0 +1,13 @@
+import { Body, Controller, Post } from '@nestjs/common';
+import { UsersService } from './users.service';
+import { CreateUserDTO } from './dto';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly userService: UsersService) {}
+
+  @Post('create-user')
+  createUsers(@Body() dto: CreateUserDTO) {
+    return this.userService.createUser(dto);
+  }
+}
---
+++ src/modules/users/users.service.ts
@@ -0,0 +1,34 @@
+import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/sequelize';
+import { User } from './models/user.model';
+import * as bcrypt from 'bcrypt';
+import { CreateUserDTO } from './dto';
+
+@Injectable()
+export class UsersService {
+  constructor(
+    @InjectModel(User) private readonly userRepository: typeof User,
+  ) {}
+
+  async hashPassword(password: string) {
+    return bcrypt.hash(password, 10);
+  }
+
+  async findUserByEmail(email: string) {
+    return this.userRepository.findOne({ where: { email } });
+  }
+
+  async createUser(dto: CreateUserDTO): Promise<CreateUserDTO> {
+    dto.password = await this.hashPassword(dto.password);
+
+    const newUser = {
+      firstName: dto.firstName,
+      username: dto.username,
+      email: dto.email,
+      password: dto.password,
+    };
+    await this.userRepository.create(newUser);
+
+    return dto;
+  }
+}
